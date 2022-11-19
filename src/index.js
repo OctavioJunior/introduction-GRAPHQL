f8bc775deab220dc50de24b395bead2d0887b15c
Octavio Chagas Junior <86677802+OctavioJunior@users.noreply.github.com>
2022-11-19 02:19:46 -0300

    :sparkles: feat: add new methods

--- src/index.js
+++ src/index.js
@@ -10,9 +10,54 @@
 
 const { ApolloServer, gql } = require("apollo-server");
 
+const users = [
+  {
+    _id: String(Math.random()),
+    name: "Octavio",
+    email: "octavio@teste.com",
+    active: true,
+  },
+  {
+    _id: String(Math.random()),
+    name: "Junior",
+    email: "junior@teste.com",
+    active: true,
+  },
+  {
+    _id: String(Math.random()),
+    name: "Chagas",
+    email: "chagas@teste.com",
+    active: false,
+  },
+];
+
 const typeDefs = gql`
+  type User {
+    # => O ponto ! serve para dizer que este campo é obrigatório
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+
+  # => É possivel fazer um aninhamento de tipos. Algo como uma relação entre os tipos
+  type Post {
+    _id: ID!
+    title: String
+    content: String
+    author: User!
+  }
+
   type Query {
     hello: String
+    users: [User!]!
+    # O ponto ! fora do array, significa que não pode retornar null ( users: null)
+    # E o ponto ! dentro do array, significa que não pode retornar um array com null ( users: [null])
+    getUserByEmail(email: String!): User!
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User!
   }
 `;
 
@@ -20,8 +65,40 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => "Hello world",
+    users: () => users,
+    getUserByEmail: (_, args) => {
+      return users.find((user) => user.email === args.email);
+    },
+  },
+
+  Mutation: {
+    createUser: (_, args) => {
+      const newUser = {
+        _id: String(Math.random()),
+        name: args.name,
+        email: args.email,
+        active: true,
+      };
+      users.push(newUser);
+      return newUser;
+    },
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => console.log(`Server started at ${url}`));
+
+/** Exemplo de aninhamento:
+
+query {
+  posts{
+    title
+    author {
+      name
+      email
+      active
+    }
+  }
+}
+Pega o post, seu titulo. O autor e seus dados!
+*/
